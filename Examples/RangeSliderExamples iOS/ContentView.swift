79adbe7a14abcbee6a563282685efc023b6b3c43
Alex Young <alex.buzov@icloud.com>
2019-08-20 15:11:00 -0700

    track and value

--- Examples/RangeSliderExamples iOS/ContentView.swift
+++ Examples/RangeSliderExamples iOS/ContentView.swift
@@ -3,13 +3,26 @@ import RangeSlider
 import SwiftUI
 
 struct ContentView: View {
-    @State var range1 = 0.0...1.0
+    @State var range = 0.2...0.8
+    @State var gradientRange = 0.0...1.0
     
     var body: some View {
-        VStack {
-            Text("Hello World")
-            RangeSlider(range: $range1)
+        NavigationView {
+            List {
+                Section(header: Text("Simple")) {
+                    RangeSlider(range: $range, valueView: DefaultValueView())
+                }
+                
+                Section(header: Text("Gradient")) {
+                    RangeSlider(range: $gradientRange, valueView:
+                        LinearGradient(gradient: Gradient(colors: [.red, .orange, .yellow, .green, .blue, .purple, .pink]), startPoint: .leading, endPoint: .trailing)
+                    )
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("Range Slider")
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
--- Sources/RangeSlider/RangeSlider.swift
+++ Sources/RangeSlider/RangeSlider.swift
@@ -1,45 +1,61 @@
 import SwiftUI
 
-public struct RangeSlider<V>: View where V : BinaryFloatingPoint, V.Stride : BinaryFloatingPoint {
+public struct RangeSlider<V, ValueView>: View where V : BinaryFloatingPoint, V.Stride : BinaryFloatingPoint, ValueView: View {
     private var range: Binding<ClosedRange<V>>
     private let boundingRange: ClosedRange<V> = 0.0 ... 1.0
     private let thumbRadius: CGFloat = 27
-
-    public init(range: Binding<ClosedRange<V>>) {
+    
+    public init(range: Binding<ClosedRange<V>>, valueView: ValueView) {
         self.range = range
+        self.valueView = valueView
     }
+    
+    private var trackView = DefaultTrackView()
+    private var valueView: ValueView
 
     public var body: some View {
         GeometryReader { geometry in
-            ZStack(alignment: .leading) {
-                RangeSliderTrack()
-//                LinearGradient(gradient: Gradient(colors: [.blue, .red]), startPoint: .leading, endPoint: .trailing)
-//                    .frame(height: 3)
-//                    .cornerRadius(1.5)
+            ZStack(alignment: .init(horizontal: .leading, vertical: .center)) {
+                self.trackView
+                    .frame(height: 3)
+                    .cornerRadius(1.5)
+                                
+                self.valueView
+                    .frame(height: 3)
+                    .cornerRadius(1.5)
+                    .mask(
+                        Rectangle()
+                            .frame(width: self.valueWidth(overallWidth: geometry.size.width), height: 3)
+                            .cornerRadius(1.5)
+                            .fixedSize()
+                            .offset(x: self.maskOffset(overallWidth: geometry.size.width))
+                )
 
                 Circle()
+                    .foregroundColor(.white)
                     .frame(width: self.thumbRadius, height: self.thumbRadius)
                     .shadow(radius: 3)
-                    .offset(x: (geometry.size.width - self.thumbRadius * 2) * CGFloat(self.range.value.lowerBound))
+                    .offset(x: self.xForLowerBound(width: geometry.size.width))
                     .gesture(
                         DragGesture().onChanged { value in
                             let newLowerBound = V(value.location.x / (geometry.size.width - self.thumbRadius * 2))
                             let validatedLowerBound = max(self.boundingRange.lowerBound, newLowerBound)
-                            let validatedUpperBound = max(validatedLowerBound, self.range.value.upperBound)
-                            self.range.value = (validatedLowerBound...validatedUpperBound).clamped(to: self.boundingRange)
+                            let validatedUpperBound = max(validatedLowerBound, self.range.wrappedValue.upperBound)
+                            self.range.wrappedValue = (validatedLowerBound...validatedUpperBound).clamped(to: self.boundingRange)
                         }
                     )
 
                 Circle()
+                    .foregroundColor(.white)
                     .frame(width: self.thumbRadius, height: self.thumbRadius)
                     .shadow(radius: 3)
-                    .offset(x: 28 + (geometry.size.width - self.thumbRadius * 2) * CGFloat(self.range.value.upperBound))
+                    .offset(x: 28 + self.xForUpperBound(width: geometry.size.width))
                     .gesture(
                         DragGesture().onChanged { value in
                             let newUpperBound = V((value.location.x - self.thumbRadius) / (geometry.size.width - self.thumbRadius * 2))
                             let validatedUpperBound = min(self.boundingRange.upperBound, newUpperBound)
-                            let validatedLowerBound = min(validatedUpperBound, self.range.value.lowerBound)
-                            self.range.value = (validatedLowerBound...validatedUpperBound).clamped(to: self.boundingRange)
+                            let validatedLowerBound = min(validatedUpperBound, self.range.wrappedValue.lowerBound)
+                            self.range.wrappedValue = (validatedLowerBound...validatedUpperBound).clamped(to: self.boundingRange)
                         }
                     )
             }
@@ -47,16 +63,40 @@ public struct RangeSlider<V>: View where V : BinaryFloatingPoint, V.Stride : Bin
         }
         .frame(height: 30)
     }
+    
+    func maskOffset(overallWidth: CGFloat) -> CGFloat {
+        let halfWidth = ((overallWidth - self.thumbRadius * 2) / 2)
+        return (xForLowerBound(width: overallWidth) - halfWidth) + valueWidth(overallWidth: overallWidth) / 2
+    }
+    
+    func valueWidth(overallWidth: CGFloat) -> CGFloat {
+        xForUpperBound(width: overallWidth) - xForLowerBound(width: overallWidth)
+    }
+    
+    func xForLowerBound(width: CGFloat) -> CGFloat {
+        (width - self.thumbRadius * 2) * CGFloat(self.range.wrappedValue.lowerBound)
+    }
+    
+    func xForUpperBound(width: CGFloat) -> CGFloat {
+        (width - self.thumbRadius * 2) * CGFloat(self.range.wrappedValue.upperBound)
+    }
 }
 
-private struct RangeSliderTrack: View {
+struct DefaultTrackView: View {
     var body: some View {
         Rectangle()
-            .frame(height: 3)
-            .cornerRadius(1.5)
             .fixedSize(horizontal: false, vertical: true)
             .foregroundColor(.secondary)
     }
+}
+
+public struct DefaultValueView: View {
+    public var body: some View {
+        Rectangle()
+            .fixedSize(horizontal: false, vertical: true)
+            .foregroundColor(.accentColor)
+    }
+    public init() {}
 }
 
 #if DEBUG
@@ -64,9 +104,7 @@ private struct RangeSliderTrack: View {
 struct RangeSlider_Previews: PreviewProvider {
     
     static var previews: some View {
-        VStack {
-            RangeSliderTrack()
-        }
+        RangeSlider(range: .constant(0...1), valueView: DefaultValueView())
     }
 }
 #endif
