5b8f50023e8d1daa70b461872d75b24917ce2a05
Alex <alex.buzov@icloud.com>
2019-08-31 01:19:59 -0700

    value view example (#43)

--- Examples/SlidersExamples/RangeSliderExamplesView.swift
+++ Examples/SlidersExamples/RangeSliderExamplesView.swift
@@ -6,7 +6,8 @@ struct RangeSliderExamplesView: View {
     @State var range2 = 1250.0...1750.0
     @State var range3 = 0.1...0.9
     @State var range4 = 0.1...0.9
-    
+    @State var range5 = 0.1...0.9
+
     var body: some View {
         ScrollView {
             RangeSlider(range: $range1, step: 0.01)
@@ -22,6 +23,22 @@ struct RangeSliderExamplesView: View {
                     LinearGradient(gradient: Gradient(colors: [.red, .orange, .yellow, .green, .blue, .purple, .pink]), startPoint: .leading, endPoint: .trailing)
                 )
                 .clippedValue(false)
+            
+            RangeSlider(
+                range: $range5,
+                trackView: Rectangle(),
+                valueView:
+                    ZStack {
+                        LinearGradient(gradient: Gradient(colors: [.blue, .red]), startPoint: .leading, endPoint: .trailing)
+                        VStack {
+                            Text("Any View").font(.largeTitle).foregroundColor(.white)
+                            Text("Place any view here and it will be masked to a selected value range").font(.title).foregroundColor(Color.white.opacity(0.5))
+                        }
+                    }
+            )
+            .height(128)
+            .thickness(128)
+            .thumbSize(CGSize(width: 8, height: 64))
         }
         .padding()
     }
