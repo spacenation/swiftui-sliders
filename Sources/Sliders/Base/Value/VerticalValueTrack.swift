7892f19c6bc05dadc1547bf6583f4d6476d51b3e
Alex <alex.buzov@icloud.com>
2020-03-12 00:19:53 -0700

    track accent color

--- Sources/Sliders/Base/Value/VerticalValueTrack.swift
+++ Sources/Sliders/Base/Value/VerticalValueTrack.swift
@@ -11,7 +11,7 @@ public struct VerticalValueTrack<V, ValueView: View, MaskView: View>: View where
     
     public var body: some View {
         GeometryReader { geometry in
-            self.view
+            self.view.accentColor(.accentColor)
                 .mask(
                     ZStack {
                         self.mask
