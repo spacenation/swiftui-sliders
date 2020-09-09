1c45c26d219e6df49b96e56452326b05751eddb9
Alex <alex.buzov@icloud.com>
2020-09-09 10:55:23 -0700

    negative frame fix (#23)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Sliders",
     platforms: [
-        .iOS(.v13), .macOS(.v10_15)
+        .iOS(.v14), .macOS(.v11)
     ],
     products: [
         .library(name: "Sliders", targets: ["Sliders"])
--- Sources/Sliders/Base/LinearRangeMath.swift
+++ Sources/Sliders/Base/LinearRangeMath.swift
@@ -5,5 +5,5 @@ import SwiftUI
 @inlinable func rangeDistance(overallLength: CGFloat, range: ClosedRange<CGFloat>, bounds: ClosedRange<CGFloat> = 0.0...1.0, lowerStartOffset: CGFloat = 0, lowerEndOffset: CGFloat = 0, upperStartOffset: CGFloat = 0, upperEndOffset: CGFloat = 0) -> CGFloat {
     let offsetLowerValue = distanceFrom(value: range.lowerBound, availableDistance: overallLength, bounds: bounds, leadingOffset: lowerStartOffset, trailingOffset: lowerEndOffset)
     let offsetUpperValue = distanceFrom(value: range.upperBound, availableDistance: overallLength, bounds: bounds, leadingOffset: upperStartOffset, trailingOffset: upperEndOffset)
-    return offsetUpperValue - offsetLowerValue
+    return max(0, offsetUpperValue - offsetLowerValue)
 }
