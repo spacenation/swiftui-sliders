8e5a6aa1e111d3f751b54db719ad75b0a665c81f
Bobby <bobby@fiskal.app>
2020-11-20 19:19:05 -0800

    chore: swift-tools 5.3

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Sliders",
-    platforms: [
-        .iOS(.v13), .macOS(.v10_15)
-    ],
     products: [
         .library(name: "Sliders", targets: ["Sliders"])
     ],
