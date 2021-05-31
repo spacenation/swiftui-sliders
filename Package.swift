74fba65b89e5766961339774fe9dc90e7fc0ff4d
Alexander Young <alex.buzov@icloud.com>
2021-05-31 14:09:54 -0700

    Swift tools version (#33)
    * chore: swift-tools 5.3
    
    * Update Package.swift
    
    Co-authored-by: Bobby <bobby@fiskal.app>
    Co-authored-by: Bobby <bobby@getfiskal.com>

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Sliders",
     platforms: [
-        .iOS(.v13), .macOS(.v10_15)
+        .iOS(.v14)
     ],
     products: [
         .library(name: "Sliders", targets: ["Sliders"])
